1d3f6415198c5b11474fa2fa34dbd13251936819
josiahprathaban <josiah.prathaban@gmail.com>
2026-02-09 13:11:03 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: false,
+      },
+      {
+        source: "/admin/",
+        destination: "/admin/index.html",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
