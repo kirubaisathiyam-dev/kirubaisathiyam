a7bb4d1f91012b348c9712d559f14422584b0e16
josiahprathaban <josiah.prathaban@gmail.com>
2026-05-01 08:30:46 +0530

    fix

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/Admin/Documents/Work/Own/kirubaisathiyam/tina/__generated__/.cache/1777602981754', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
